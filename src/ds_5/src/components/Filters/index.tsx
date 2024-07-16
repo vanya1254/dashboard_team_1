4d09845aec7765f38da13a0b2e38281a55f48885
Ivan <vanya1254@yandex.ru>
2024-07-16 19:33:20 +0300

    feat: add layout filters

--- src/ds_5/src/components/Filters/index.tsx
+++ src/ds_5/src/components/Filters/index.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { FaSortAlphaDown } from 'react-icons/fa';
+import { IoSearch } from 'react-icons/io5';
 
 import { SelectCustom } from '../SelectCustom';
 
 import styles from './Filters.module.scss';
 
+const posts = [];
+const divisions = [];
+
 export const Filters: React.FC = () => {
   return (
     <div className={styles.root}>
-      <input type="text" />
-      <button>
+      <input className={styles.root_search} type="text" placeholder="Поиск по ФИО" />
+      <button className={styles.root_btn}>
         <FaSortAlphaDown />
       </button>
-      <SelectCustom onClickFilter={(filter) => {}} selectTitle={'Должность'} options={[]} />
-      <SelectCustom onClickFilter={(filter) => {}} selectTitle={'Подразделение'} options={[]} />
-      <button>Поиск</button>
+      <SelectCustom onClickFilter={(filter) => {}} selectTitle={'Должность'} options={posts} />
+      <SelectCustom onClickFilter={(filter) => {}} selectTitle={'Подразделение'} options={divisions} />
+      <button className={styles.root_btn}>
+        <IoSearch />
+      </button>
     </div>
   );
 };
